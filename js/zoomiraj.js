2f4c45bcca948d25edce53e997ef25b8f048285a
user.email <tspinjac@gmail.com>
2017-12-09 19:16:07 +0100

    Vlaho3

--- js/zoomiraj.js
+++ js/zoomiraj.js
@@ -1,8 +1,5 @@
-$('#zoom_01').elevateZoom({
-    zoomType        : "lens",
-    lensShape : "round",
-    lensSize    : 150,
-    responsive:true,
-    borderColour: "lime"
-
-   }); 
+$("#zoom_01").elevateZoom({
+  zoomType				: "lens",
+  lensShape : "round",
+  lensSize    : 200
+});
